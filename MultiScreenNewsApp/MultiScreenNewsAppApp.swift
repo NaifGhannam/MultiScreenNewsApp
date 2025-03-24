19a6d2d4843b1df8b916bee13de828d6cde02343
Naif Ghannam <dhdfaui@gmail.com>
2025-03-25 00:04:51 +0300

    Initial Commit

---
+++ MultiScreenNewsApp/MultiScreenNewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiScreenNewsAppApp.swift
+//  MultiScreenNewsApp
+//
+//  Created by Mac on 25/09/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct MultiScreenNewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
